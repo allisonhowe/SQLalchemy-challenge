29828d73e4947903746d10a38ccb6552b536a77c
Allison Howe <allisonhowe77@gmail.com>
2020-08-26 23:09:12 -0400

    final update

--- climate_app.py
+++ climate_app.py
@@ -20,8 +20,6 @@ Base.prepare(engine, reflect=True)
 measurement = Base.classes.measurement
 station = Base.classes.station
 
-session = Session(engine)
-
 #################################################
 # Flask Setup
 #################################################
@@ -36,12 +34,12 @@ app = Flask(__name__)
 def home():
     """List all available api routes."""
     return (
-        f"Available Routes in the Hawaii Weather API:<br/>"
-        f"/api/v1.0/precipitation<br/>"
-        f"/api/v1.0/stations<br/>"
-        f"/api/v1.0/tobs<br/>"
-        f"/api/v1.0/<start><br/>"
-        f"/api/v1.0/<start>/<end>")
+        f"Available Routes in the Hawaii Weather API:<br/><br/>"
+        f"Returns a list of precipitation data from the last year: /api/v1.0/precipitation<br/><br/>"
+        f"Returns the list of Hawaii stations used in this dataset: /api/v1.0/stations<br/><br/>"
+        f"Returns the daily temperature observations made by the most active station (station USC00519281): /api/v1.0/tobs<br/><br/>"
+        f"Returns the minimum, average,and maximum temperature recorded for each day, beginning with the date entered (enter as yyyy-mm-dd): /api/v1.0/startdate<br/><br/>"
+        f"Returns the minimum, average,and maximum temperature recorded for each day between the 2 dates entered (enter as yyyy-mm-dd): /api/v1.0/startdate/enddate")
 
 
 @app.route("/api/v1.0/precipitation")
@@ -92,8 +90,48 @@ def tobs():
     return jsonify(temps)
 
 
-# @app.route("/api/v1.0/<start>")
+@app.route("/api/v1.0/<start>")
+def start_only(start):
+    """ Return a JSON list of the minimum temperature, the average temperature, and the max temperature for a given start
+    range. Calculate `TMIN`, `TAVG`, and `TMAX` for all dates greater than and equal to the start date."""
+    session = Session(engine)
+    start_date = dt.date.fromisoformat(start)
+    start_temps = session.query(measurement.date, func.min(measurement.tobs), func.avg(measurement.tobs), func.max(measurement.tobs)).\
+        filter(measurement.date >= start_date).\
+        group_by(measurement.date).all()
+    session.close()
+    temps = []
+    for date, min_tobs, avg_tobs, max_tobs in start_temps:
+        temp_dict = {}
+        temp_dict["date"] = date
+        temp_dict["minimum_temp"] = min_tobs
+        temp_dict["average_temp"] = avg_tobs
+        temp_dict["maximum_temp"] = max_tobs
+        temps.append(temp_dict)
+    return jsonify(temps)
 
+
+@app.route("/api/v1.0/<start>/<end>")
+def start_end(start,end):
+    """ Return a JSON list of the minimum temperature, the average temperature, and the max temperature for a given start end
+    range. Calculate `TMIN`, `TAVG`, and `TMAX` for dates between the start and end date inclusive."""
+    session = Session(engine)
+    start_date = dt.date.fromisoformat(start)
+    end_date = dt.date.fromisoformat(end)
+    start_end_temps = session.query(measurement.date, func.min(measurement.tobs), func.avg(measurement.tobs), func.max(measurement.tobs)).\
+        filter(measurement.date >= start_date).\
+        filter(measurement.date <= end_date).\
+        group_by(measurement.date).all()
+    session.close()
+    temps = []
+    for date, min_tobs, avg_tobs, max_tobs in start_end_temps:
+        temp_dict = {}
+        temp_dict["date"] = date
+        temp_dict["minimum_temp"] = min_tobs
+        temp_dict["average_temp"] = avg_tobs
+        temp_dict["maximum_temp"] = max_tobs
+        temps.append(temp_dict)
+    return jsonify(temps)
 
 
 if __name__ == '__main__':
